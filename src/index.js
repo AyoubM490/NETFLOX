a198d03c1a0647091d19dc67f26f8e8cdac36c63
ayubch <ayoub.chahir96@gmail.com>
2022-03-08 12:05:10 +0100

    Add Items

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './css-reset.css';
 import './style.css';
+import getShows from './modules/getShows.js';
+import buildShows from './modules/buildShows.js';
+
+const items = document.querySelector('.items');
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const shows = await getShows();
+  const sixShows = shows.slice(0, 6);
+
+  buildShows(sixShows, items);
+});
