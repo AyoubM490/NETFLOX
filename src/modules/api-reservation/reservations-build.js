2d870724c1a566e8a330622d022d412247ffad20
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-10 23:06:40 +0200

    Add reservation dom manipulation function

---
+++ src/modules/api-reservation/reservations-build.js
@@ -0,0 +1,21 @@
+import getreservations from './get-reservations.js';
+
+const reservationBuild = async (containerClass, url, id, itemID) => {
+  const container = document.querySelector(`.${containerClass}`);
+  const data = await getreservations(url, id, itemID);
+  if (data.length === 0) {
+    container.innerHTML = '<span class="res-item-default">no reservations history!</span>';
+  } else {
+    container.innerHTML = '';
+    data.forEach((item) => {
+      const div = document.createElement('div');
+      div.classList.add('res-item-group');
+      div.innerHTML += `<span class="res-item-name">${item.username} reserved this item</span>
+            <span class="res-item-startdate">from: ${item.date_start}</span>
+            <span class="res-item-enddate">to: ${item.date_end}</span>`;
+      container.appendChild(div);
+    });
+  }
+};
+
+export default reservationBuild;
