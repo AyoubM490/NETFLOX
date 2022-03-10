b1e5f48f36993342f26c90944d68846af98f5127
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-10 22:53:40 +0200

    Add comment skeleton to popup && link comment builder and listener functions

--- src/modules/popup-build.js
+++ src/modules/popup-build.js
@@ -1,73 +1,117 @@
 import { starRating, getStars } from './user-star-rating.js';
+import commentBuild from './api-comment/comments-build.js';
+import addCommentListener from './api-comment/add-com-listener.js';
 
-const insertPopup = (popupContainer, modalContainer, dataArray, index) => {
+const apiURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const sessionID = 'GPPQtj8HcYdkGNz2Bewy';
+
+const insertPopup = async (
+  popupContainer,
+  modalContainer,
+  dataArray,
+  index,
+) => {
   modalContainer.innerHTML = '';
   const dataElement = dataArray[index - 1];
   const modal = document.createElement('ul');
   modal.classList.add('item-container');
   modal.setAttribute('item', `${index}`);
-  modal.innerHTML = `<li class="item-description-container">
-    <ul class="item-description">
-      <li class="company-likes-container">
-        <ul class="company-likes">
-            <li class="likes">
-                <i class="bx bx-heart bx-xs bx-burst-hover"></i>
-              </li>
-            <li class="company">${dataElement.network.name}</li>
+  modal.innerHTML = `        <li class="item-description-container">
+  <ul class="item-description">
+    <li class="company-likes-container">
+      <ul class="company-likes">
+          <li class="likes">
+              <i class="bx bx-heart bx-xs bx-burst-hover"></i>
+            </li>
+          <li class="company">${dataElement.network.name}</li>
 
-        </ul>
-      </li>
-      <li class="item-title"><a href="${dataElement.url}">${
-  dataElement.name
+      </ul>
+    </li>
+    <li class="item-title"><a href="${dataElement.url}">${
+dataElement.name
 }</a> </li>
-      <li class="item-genres-container">
-        <ul class="item-genres">
-        </ul>
-      </li>
-      <li class="item-details-container">
-        <ul class="item-details">
-          <li class="started">Premiered: ${dataElement.premiered}</li>
-          <li class="status">${dataElement.status} ${
-  dataElement.status === 'Ended' ? `: ${dataElement.ended}` : ''
+    <li class="item-genres-container">
+      <ul class="item-genres">
+      </ul>
+    </li>
+    <li class="item-details-container">
+      <ul class="item-details">
+        <li class="started">Premiered: ${dataElement.premiered}</li>
+        <li class="status">${dataElement.status} ${
+dataElement.status === 'Ended' ? `: ${dataElement.ended}` : ''
 }</li>
-        </ul>
-        <ul class="rating-container">
-            <li class="item-rating"><span class="imdb">IMDB: </span><span>(${
-  dataElement.rating.average / 2
+      </ul>
+      <ul class="rating-container">
+          <li class="item-rating"><span class="imdb">IMDB: </span><span>(${
+dataElement.rating.average / 2
 }<i class="fas fa-star"></i>)</span></li>
-            <li class="user-rating">
-              <div class="star-rating">
-                <div class="post">
-                  <div class="rating-feedback-text">
-                    Thanks for your rating!
-                  </div>
-                </div>
-                <div class="stars-container" item="${index}">
-                    <p class="user-review">Your review:</p>
-                  <input type="radio" name="rating" id="r-5" />
-                  <label for="r-5" class="fas fa-star"></label>
-                  <input type="radio" name="rating" id="r-4" />
-                  <label for="r-4" class="fas fa-star"></label>
-                  <input type="radio" name="rating" id="r-3" />
-                  <label for="r-3" class="fas fa-star"></label>
-                  <input type="radio" name="rating" id="r-2" />
-                  <label for="r-2" class="fas fa-star"></label>
-                  <input type="radio" name="rating" id="r-1" />
-                  <label for="r-1" class="fas fa-star"></label>
+          <li class="user-rating">
+            <div class="star-rating">
+              <div class="post">
+                <div class="rating-feedback-text">
+                  Thanks for your rating!
                 </div>
               </div>
+              <div class="stars-container" item="${index}">
+                  <p class="user-review">Your review:</p>
+                <input type="radio" name="rating" id="r-5" />
+                <label for="r-5" class="fas fa-star"></label>
+                <input type="radio" name="rating" id="r-4" />
+                <label for="r-4" class="fas fa-star"></label>
+                <input type="radio" name="rating" id="r-3" />
+                <label for="r-3" class="fas fa-star"></label>
+                <input type="radio" name="rating" id="r-2" />
+                <label for="r-2" class="fas fa-star"></label>
+                <input type="radio" name="rating" id="r-1" />
+                <label for="r-1" class="fas fa-star"></label>
+              </div>
+            </div>
+          </li>
+        </ul>
+    </li>
+    <li class="item-summary">${dataElement.summary}</li>
+
+
+    <li class="com-res-container">
+    <ul class="com-res">
+    <li class="com-res-header-container">
+      <ul class="com-res-header">
+        <li class="com-header"><i class='bx bx-tada-hover bx-xs bx-message-square-dots'></i> comments <span class="com-count">3</span></li>
+        <li class="res-header"><i class='bx bx-tada-hover bx-task bx-xs' ></i> reservations <span class="com-count">4</span></li>
+      </ul>
+    </li>
+    <li class="com-container">
+      <ul class="com">
+        <li class="com-history-container">
+          <ul class="com-history">
+            <li class="com-item">
+              
             </li>
           </ul>
-      </li>
-      <li class="item-summary">${dataElement.summary}</li>
-    </ul>
-  </li>
-  <li class="item-img">
-    <img
-      src="${dataElement.image.original}"
-      alt="show-poster"
-    />
-  </li>`;
+        </li>
+        <li class="com-add">
+          <form action="#" class="com-form">
+            <input type="text" name="name" placeholder="Your name." required>
+            <input type="text" name="message"  rows="2" placeholder="Leave a comment..." required>
+            <button type="submit" class="com-form-btn">Comment</button>
+          </form>
+        </li>
+      </ul>
+    </li>
+
+  </ul>
+  
+    </li>
+
+  </ul>
+</li>
+<li class="item-img">
+  <img
+    src="${dataElement.image.original}"
+    alt="show-poster"
+  />
+</li>
+ `;
 
   modalContainer.appendChild(modal);
   popupContainer.style.display = 'block';
@@ -84,6 +128,20 @@ const insertPopup = (popupContainer, modalContainer, dataArray, index) => {
     genresContainner.innerHTML += `<li class="genre">${element}</li>`;
   });
   starRating(stars, post, index);
+
+  // insert comments
+  await commentBuild('com-item', apiURL, sessionID, dataElement.name);
+
+  // comment event listener
+  await addCommentListener(
+    'com-item',
+    commentBuild,
+    apiURL,
+    sessionID,
+    dataElement.name,
+    'com-form',
+  );
+
 };
 
 export default insertPopup;
