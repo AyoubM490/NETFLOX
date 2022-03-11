0e98e15256e72606c3eea2069bb102f200f760f6
ayubch <ayoub.chahir96@gmail.com>
2022-03-11 09:33:43 +0100

    Add test for the showsCount function

---
+++ src/modules/test/showsCount.test.js
@@ -0,0 +1,26 @@
+import { showsCount } from '../counters.js';
+
+test('returns 6 when length of array is 6', () => {
+  const list = [
+    { item: 3 },
+    { item: 4 },
+    { item: 5 },
+    { item: 1 },
+    { item: 2 },
+    { item: 10 },
+  ];
+  const count = showsCount(list);
+  expect(count).toBe(6);
+});
+
+test('returns 1 when array length is 1', () => {
+  const list = [{ item: 4 }];
+  const count = showsCount(list);
+  expect(count).toBe(1);
+});
+
+test('returns 0 when array is empty', () => {
+  const list = [];
+  const count = showsCount(list);
+  expect(count).toBe(0);
+});
