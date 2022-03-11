f854ff60840c3e982546a40a84a1c247942bc22e
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-11 04:28:02 +0200

    Modify reservation count test

--- src/modules/test/reservation-count.test.js
+++ src/modules/test/reservation-count.test.js
@@ -2,7 +2,6 @@ import reservationCount from "../api-reservation/reservation-count.js"
 
 const dataArray = [1, 2];
 const item = document.createElement('div');
-item.classList.add('item-class');
 
 describe('Testing comment count function', () => {
   test('count on array', () => {
